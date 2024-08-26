a5503ef54b3d7650285dbbfca1f2be17939e2638
havetc <corentin.havet@hotmail.fr>
2024-08-26 18:37:26 +0200

    [FEAT] JSON constrained support (#1125)
    Co-authored-by: Yineng Zhang <me@zhyncs.com>

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -10,6 +10,7 @@ suites = {
         "test_eval_accuracy_mini.py",
         "test_large_max_new_tokens.py",
         "test_openai_server.py",
+        "test_json_constrained.py",
         "test_skip_tokenizer_init.py",
         "test_torch_compile.py",
         "test_triton_attn_backend.py",
